e0f1d31d90e6c702ef042f639a61c6158e17ec3d
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2025-05-27 15:30:53 +0530

    BubbleSort.py

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,5 +1,4 @@
 class Solution:
-    #Function to sort the array using bubble sort algorithm.
     def bubbleSort(self,arr):
        for i in range(0,len(arr) - 1) :
            for j in range(0,len(arr) - i - 1) :
